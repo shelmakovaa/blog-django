19b115afe9023af2836380a5a520751b7113ff20
Alena Shelmakova <alena0888@gmail.com>
2023-05-04 19:05:31 +0300

    add content to start page

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,53 @@ from django.shortcuts import render
 
 
 def start_page(request):
-    return render(request, 'blog/index.html')
+    posts = [
+        {
+            'title': 'Рыбалка',
+            'description': 'Хорошо посидели',
+            'date': '21 авг 2021',
+            'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'
+        },
+        {
+            'title': 'Париж',
+            'description': 'Незабываемое путешествие',
+            'date': '5 сент 2020',
+            'content': '''Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'''
+        },
+        {
+            'title': 'Финал лиги чемпионов',
+            'description': 'Реал опять выиграл ЛЧ',
+            'date': '28 мая 2022',
+            'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, rem.'
+        },
+        {
+            'title': 'Охота на уток',
+            'description': 'Ни одна утка не пострадала',
+            'date': '7 дек 2019',
+            'content': 'Lorem ipsum dolor sit amet.'
+        },
+        {
+            'title': 'Фестиваль огурца',
+            'description': 'Суздаль ждет тебя',
+            'date': '12 июль 2021',
+            'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consectetur id inventore odit recusandae!'
+        },
+        {
+            'title': 'Нашествие',
+            'description': 'Даешь рок, но в следующем году',
+            'date': '29 июль 2021',
+            'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est mollitia recusandae rem?'
+        },
+        {
+            'title': 'Новый год',
+            'description': 'Эх, еще один год пролетел',
+            'date': '31 дек 2022',
+            'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto corporis fuga ipsam laboriosam, nesciunt non quae qui ut veniam.'
+        },
+    ]
+
+    return render(request, 'blog/index.html', context={'posts': posts})
 
 
 def all_posts(request):
